21765ca876d1ff615fbee34cec0485370c0cc3a3
koraniar <eganfs@gmail.com>
2017-06-24 21:12:08 -0500

    Upload project

---
+++ Assets/Scripts/ActualizarValoresGameOver.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActualizarValoresGameOver : MonoBehaviour {
+
+	public TextMesh total;
+	public TextMesh record;
+	public Puntuacion puntuacion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnEnable(){
+		total.text = puntuacion.puntuacion.ToString();
+		record.text = EstadoJuego.estadoJuego.puntuacionMaxima.ToString();
+	}
+}
---
+++ Assets/Scripts/EstadoJuego.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+
+public class EstadoJuego : MonoBehaviour {
+
+	public int puntuacionMaxima = 0;
+
+	public static EstadoJuego estadoJuego;
+
+	void Awake(){
+		if(estadoJuego==null){
+			estadoJuego = this;
+			DontDestroyOnLoad(gameObject);
+		}else if(estadoJuego!=this){
+			Destroy(gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		Cargar();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void Guardar(){
+		PlayerPrefs.SetInt("puntuacionMaxima", estadoJuego.puntuacionMaxima);
+	}
+	
+	void Cargar(){
+		puntuacionMaxima = PlayerPrefs.GetInt("puntuacionMaxima", 0);
+	}
+}
